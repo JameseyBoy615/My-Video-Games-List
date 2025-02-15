8ebd462b041a1729dfeeee01ec78f4558d1a3300
James Palumbo <jamesmpalumbo1@gmail.com>
2025-02-15 18:44:51 -0500

    set up user, lists, games Schemas

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const gamesSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  review: {
+    type: String,
+  },
+});
+
+const listsSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  games: [gamesSchema],
+});
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  applications: [listsSchema],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
