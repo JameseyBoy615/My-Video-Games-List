cce1db12705970a2e1ed067b5858e4497e9a9fcd
James Palumbo <jamesmpalumbo1@gmail.com>
2025-02-15 19:17:29 -0500

    set up auth conditions and .ejs pages

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
 // Middleware
 
-const port = process.env.PORT ? process.env.PORT : "3000";
+const authController = require("./controllers/auth.js");
+
+const port = process.env.PORT ? process.env.PORT : "3003";
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -18,9 +22,32 @@ mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passUserToView);
+
 // Routes
 
+app.get("/", (req, res) => {
+  if (req.session.user) {
+    res.redirect(`/users/${req.session.user._id}/lists`);
+  } else {
+    res.render("index.ejs");
+  }
+});
+
 // Ports
+
+app.use("/auth", authController);
+app.use(isSignedIn); // use new isSignedIn middleware here
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
