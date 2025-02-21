28eb6d233a77ca7f3ce9d8d824768ec1d622486f
James Palumbo <jamesmpalumbo1@gmail.com>
2025-02-20 20:58:48 -0500

    added explore community pages/ added css to pages

---
+++ controllers/users.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.render("users/index.ejs", {
+      users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+
+    res.render("users/show.ejs", { user });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:userId/:listId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+
+    const list = user.lists.id(req.params.listId);
+
+    res.render("users/lists/show.ejs", {
+      user,
+      list: list,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+module.exports = router;
